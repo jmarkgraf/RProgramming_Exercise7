b858ac17fb5a26de828cb7f8540b1b5530e2d5d4
jmarkgraf <jonas.markgraf@gmail.com>
2017-02-16 11:34:12 -0600

    Change of design and comments; multiple questions still open.

--- ClassExercise9.R
+++ ClassExercise9.R
@@ -1,8 +1,10 @@
 # R Programming
 # Exercise 5: The Sorting Hat
-# Feb 14, 2017
+# Feb 14 - 16, 2017
+# Author: Jonas Markgraf
 # -------------------------------
 
+rm(list = ls())
 
 # 1) students --------------------
 
@@ -17,6 +19,7 @@ Harry <- funcStudent("Harry Potter")
 
 # 2) sorter ----------------------
 
+# generate random 4x4 matrix
 my_matrix <- matrix(sample(1:100, 16), nrow = 4, ncol = 4)
 
 # different ways to unlist
@@ -25,9 +28,24 @@ b <- unlist(Harry)[2:5]
 
 # function
 sorter <- function(student_name, my_matrix = diag(4)) {
+  # Function name: sorter()
+  # Purpose: evaluate values of 'student' object, assign students to groups based on values.
+  # Function: 
+  ##  1) takes values from output in list format, turns them into numerics (matrix), and unlists to vector 'a'
+  ##  2) multiplies vector 'a' with 4x4 matrix (by default identity matrix)
+  ##  3) identifies maximum value in newly created vector and prints group labels
+  ##  User option to only calculate m, d or both statistics
+  # Args:
+  ##  student_name: object of the class "student"
+  ##  my_matrix: by default 4x4 identity matrix
+  # Open questions:
+  ## - what is the role of "my_matrix"? 
+  ### - where does the matrix come from? I set it to the identity matrix by default...
+  ### - why do we need to multiply it with "a"?
+  ## - we didn't use the "sort" function yet although we were supposed to do that...
+  # Author: Jonas Markgraf
   a <- unlist(matrix(student_name[2:5], ncol = 1, dimnames = list(names(student_name[2:5]))))
   vect1 <- a %*% t(my_matrix)
-  print(sort(vect1, decreasing = T))
   if(max(vect1) == vect1[1]) {
     print("GRIFFINDOR!")
   } else if(max(vect1) == vect1[2]) {
@@ -39,11 +57,9 @@ sorter <- function(student_name, my_matrix = diag(4)) {
   }
 }
 
+# Examples:
+
 sorter(Harry)
 
-# my questions:
-## - what is the role of "my_matrix"? 
-### - where does the matrix come from? I set it to the identity matrix by default...
-### - why do we need to multiply it with "a"?
-## - we didn't use the "sort" function yet although we were supposed to do that...
-## - 
+sorter(Harry, my_matrix = my_matrix)
+
