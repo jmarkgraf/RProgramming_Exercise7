8af5ea202f8270146d963f31ba48162236cbe2c0
jmarkgraf <jonas.markgraf@gmail.com>
2017-02-21 13:16:55 -0600

    First Attempts on "argus_filch": no solution

--- ClassExercise9.R
+++ ClassExercise9.R
@@ -16,6 +16,8 @@ funcStudent <- function(name) {
 }
 
 Harry <- funcStudent("Harry Potter")
+Hermine <- funcStudent("Hermine")
+Ron <- funcStudent("Ron")
 
 # 2) sorter ----------------------
 
@@ -24,10 +26,11 @@ my_matrix <- matrix(sample(1:100, 16), nrow = 4, ncol = 4)
 
 # different ways to unlist
 a <- unlist(matrix(Harry[2:5], ncol = 1, dimnames = list(names(Harry[2:5]))))
+a
 b <- unlist(Harry)[2:5]
 
 # function
-sorter <- function(student_name, my_matrix = diag(4)) {
+sort.student <- function(student_name, my_matrix = diag(4)) {
   # Function name: sorter()
   # Purpose: evaluate values of 'student' object, assign students to groups based on values.
   # Function: 
@@ -37,7 +40,7 @@ sorter <- function(student_name, my_matrix = diag(4)) {
   ##  User option to only calculate m, d or both statistics
   # Args:
   ##  student_name: object of the class "student"
-  ##  my_matrix: by default 4x4 identity matrix
+  ##  my_matrix: must be a 4x4 data frame; by default 4x4 identity matrix
   # Open questions:
   ## - what is the role of "my_matrix"? 
   ### - where does the matrix come from? I set it to the identity matrix by default...
@@ -57,9 +60,92 @@ sorter <- function(student_name, my_matrix = diag(4)) {
   }
 }
 
+# alternative solution from JB (shorter!):
+sort.student <- function(student_name, my_matrix = diag(4)) {
+  a <- unlist(matrix(student_name[2:5], ncol = 1, dimnames = list(names(student_name[2:5]))))
+  vect1 <- a %*% t(my_matrix)
+  houses <- c("Grif", "Slyth", "Rav", "Huf")
+  return(houses[which.max(vect1)])
+}
 # Examples:
 
-sorter(Harry)
+sort.student(Harry)
+sort(Harry)
 
-sorter(Harry, my_matrix = my_matrix)
+# 3) Modifications -------------------
 
+sort.mod <- function(student_name, my_matrix = diag(4)) {
+  a <- unlist(matrix(student_name[2:5], ncol = 1, dimnames = list(names(student_name[2:5]))))
+  vect1 <- a %*% t(my_matrix)
+  if(max(vect1) == vect1[1]) {
+    class(student_name) <- c(class(student_name), "Gryffindor")
+  } else if(max(vect1) == vect1[2]) {
+    class(student_name) <- c(class(student_name), "Slytherin")
+  } else if(max(vect1) == vect1[3]) {
+    class(student_name) <- c(class(student_name), "Ravenclaw")
+  } else {
+    class(student_name) <- c(class(student_name), "Hufflepuff")
+  }
+  return(student_name)
+}
+
+# Examples:
+Harry <- sort.mod(Harry)
+
+# alternative solution (JB) - shorter!:
+
+sort.mod <- function(student_name, my_matrix = diag(4)) {
+  a <- unlist(matrix(student_name[2:5], ncol = 1, dimnames = list(names(student_name[2:5]))))
+  vect1 <- a %*% t(my_matrix)
+  houses <- c("Grif", "Slyth", "Rav", "Huf")
+  class(student_name) <- c(class(student_name), houses[which.max(vect1)])
+  print(paste0[which.max(vect1)], "!")
+  return(student_name)
+}
+
+# 4) Curfew -----------------------------
+
+Gryffindor_Tower <- new.env()
+Black_Lake <- new.env()
+Ravenclaw_Tower <- new.env()
+Basement <- new.env()
+
+curfew <- function(student_name) {
+  if("Gryffindor" %in% class(student_name)) {
+    assign(deparse(substitute(student_name)), student_name, envir = Gryffindor_Tower)
+  } else if("Slytherin" %in% class(student_name)) {
+    assign(deparse(substitute(student_name)), student_name, envir = Black_Lake)
+  } else if("Ravenclaw" %in% class(student_name)) {
+    assign(deparse(substitute(student_name)), student_name, envir = Ravenclaw_Tower)
+  } else {
+    assign(deparse(substitute(student_name)), student_name, envir = Basement)
+  }
+}
+
+# Examples:
+curfew(Harry)
+ls(Gryffindor_Tower)
+
+# write function method "argus_filch" ---------------------------------------------
+## collects all objects of class "student" in global environment
+## runs "curfew" function on them
+## "curfew" function must run in global environment
+
+# basic structure
+eval(parse(text=thisCall), envir = globalenv())
+
+# test examples
+parse(text = class(Harry))
+eval(parse(text = class(Harry)))  # need to write functions for "student" and houses
+
+
+argus_filch <- function() {
+  student <- function(x){
+    curfew(x)
+  }
+  
+}
+
+# failed attempts
+ClassElements <- Filter( function(x) "student" %in% class(get(x)), ls())
+ClassElements  # only gives character string with names of elements of class "student"...
